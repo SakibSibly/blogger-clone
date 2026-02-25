ab517314a46329cf185ae609c2dc885453165a6b
SakibSibly <sibly@duck.com>
2026-02-25 12:19:09 +0600

    Update "Protected route implemented"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './routes/ProtectedRoute';
+import Layout from './Layouts/Layout';
+import Home from './components/Home/Home';
+import LandingPage from './components/LandingPage/LandingPage';
+import Login from './components/Login/Login';
+import BlogCreation from './components/BlogCreation/BlogCreation';
+
 import './App.css'
+
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/create-blog" element={<BlogCreation />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
